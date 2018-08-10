926a3b1a1c439c552a98990cfc8d4198fdc01b34
Thalia <thalia.brito@rutgers.edu>
2018-08-10 00:50:26 -0400

    Create basic template

---
+++ myscript.js
@@ -0,0 +1,36 @@
+let  menuOpenBtn= document.getElementById('menu-open-btn');
+let menuCloseBtn = document.getElementById('menu-close-btn');
+let menuList = document.getElementById('menu-list');
+
+function toggleMenu(){
+	if(menuOpenBtn.classList.contains('show-inline')){
+		menuOpenBtn.classList.remove('show-inline');
+		menuCloseBtn.classList.remove('hide');
+		menuOpenBtn.classList.add('hide');
+		menuCloseBtn.classList.add('show-inline');
+		menuList.className = "show";
+	}else{
+		menuOpenBtn.classList.remove('hide');
+		menuCloseBtn.classList.remove('show-inline');
+		menuOpenBtn.classList.add('show-inline');
+		menuCloseBtn.classList.add('hide');
+		menuList.className = "hide";
+	}
+}
+
+
+function scrollToSection(id){
+	let elmnt = document.getElementById(id)
+	elmnt.scrollIntoView({behavior:"smooth",block:"start"});
+	toggleMenu();
+	/*let mobilemenu = document.getElementById('mobile-menu-links');
+	if(mobilemenu.style.display === "block"){
+		toggle();
+		mobilemenu.style.display = "none";
+	}*/
+}
+function scrollToSectionNoToggle(id){
+	let elmnt = document.getElementById(id)
+	elmnt.scrollIntoView({behavior:"smooth",block:"start"});
+}
+

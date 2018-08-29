3149c63a0c65951d1743f29d22c18195e20fc0d3
Thalia <thalia.brito@rutgers.edu>
2018-08-28 22:37:25 -0400

    update colors

--- myscript.js
+++ myscript.js
@@ -48,14 +48,24 @@ t.to('#body', 1, {
         autoAlpha: 1,
         ease: Power1.easeInOut
     })
-	.to('#ring', 1, {
+	.to('#ring', 0.5, {
 		autoAlpha:1,
         strokeDashoffset:0,
         ease: Power1.easeInOut,
         delay:1
     })
-    .to('#body,#ring',1,{autoAlpha:0, ease:Power1.easeOut},"-=0.5")
-    .to('#loading-page',1.5,{autoAlpha:0,display:"none",ease:Power1.easeOut},"-=0.5")
+    .to('#body,#ring',0.8,{autoAlpha:0, ease:Power1.easeOut},"-=0.5")
+   /* .to('#loading-page',1.5,{autoAlpha:0,display:"none",ease:Power1.easeOut},"-=0.5")*/
+	.fromTo('#loading-page',0.1,{top:0},{top:'-20vh', ease:Power1.easeOut})
+	.to('#loading-page, #loading-backdrop', 0.5,/*{top:0},*/{top:'-100vh'/*, ease:Power1.easeInOut*/})
+	.to('#main-header-cover',0.4,{height:0, transformOrigin:"bottom", ease:Power2.easeOut},'-=0.2')
+	.fromTo('#home-main-letters > path',0.5,{autoAlpha:0},{autoAlpha:1,ease:Power2.easeInOut},'-=0.3')
+	.fromTo('#home-subheader',0.5,{autoAlpha:0},{autoAlpha:1,ease:Power2.easeInOut},'-=0.3')
+	.fromTo('#home-navbar > li', 0.5,{autoAlpha:0,paddingTop:"2em"},{autoAlpha:1,paddingTop:0})
+	/*.to('#loading-backdrop',0.5,{scaleY:0,ease:Power2.easeOut})*/
+/*var tl = new TimelineMax('loading-backdrop',0.3,{ repeat: 1, yoyo: true, scaleY:3,ease:ExpoScaleEase(zoom,1,Power1.easeOut) },0)
+  .to(test._mask, 1, { scaleY: 3, ease: ExpoScaleEase(zoom, 1, Power1.easeOut) }, 0)
+ 	*/
 
 let selected;
 
